a7efb92f455638f72b64b9a9279e5187de4dbe01
Jamshid Ganiev <112170134+jackhuntersys@users.noreply.github.com>
2025-04-14 21:46:12 +0900

    Update car_price.py

--- 3-oy/4dars/car_price.py
+++ 3-oy/4dars/car_price.py
@@ -5,6 +5,6 @@ model = load('car_price_model.joblib')
 
 
 
-st.set_page_config(page_title='Yomgirni bashorat qilish uchun app',layout='centered')
-st.title('Ob xavo:Yomgir')
-st.markdown('Bugungi ob xavo malumotlarini kiriting va yomgirni bizning app yordamida bashorat qiling!')
+st.set_page_config(page_title=' app',layout='centered')
+st.title('bu app')
+st.markdown('bu markdown!')
