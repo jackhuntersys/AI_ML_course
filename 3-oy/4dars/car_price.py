b362da826c7e7b45421caf7d7be7bac6823aa1e8
Jamshid Ganiev <jrganiev@gmail.com>
2025-04-21 22:11:21 +0900

    555

--- 3-oy/4dars/car_price.py
+++ 3-oy/4dars/car_price.py
@@ -4,16 +4,15 @@ import os
 from joblib import load
 from sklearn.metrics import accuracy_score
 
-model = load('https://github.com/jackhuntersys/AI_ML_course/blob/25a1f70bd658d096fd3a79bbbfc691e77626d90a/3-oy/4dars/model.joblib')
+model = load(r'model.joblib')
+if model:
+    print('true')
 
 
 st.set_page_config(page_title=' app',layout='centered')
 st.title('bu app emas fffffff')
 st.markdown('bu markdown!')
-MODEL_PATH = 'car_price.joblib'
 
-if os.path.exists(MODEL_PATH):
-    model = load(MODEL_PATH)
-else:
-    st.error(f"Model file '{MODEL_PATH}' not found. Please upload it to continue.")
-    st.stop()
+
+print("Current dir:", os.getcwd())
+print("Files:", os.listdir('.'))
