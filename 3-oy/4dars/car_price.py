238d80835eb669a01c92f1fc99e1e004f4e4d027
Jamshid Ganiev <jrganiev@gmail.com>
2025-04-21 20:35:11 +0900

    hh

--- 3-oy/4dars/car_price.py
+++ 3-oy/4dars/car_price.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import os
 from joblib import load
 from sklearn.metrics import accuracy_score
 
@@ -9,3 +10,10 @@ model = load('car_price.joblib')
 st.set_page_config(page_title=' app',layout='centered')
 st.title('bu app emas fffffff')
 st.markdown('bu markdown!')
+MODEL_PATH = 'car_price.joblib'
+
+if os.path.exists(MODEL_PATH):
+    model = load(MODEL_PATH)
+else:
+    st.error(f"Model file '{MODEL_PATH}' not found. Please upload it to continue.")
+    st.stop()
