ee69adba51ad2c762ca879bce05a074a0560ae98
Jamshid Ganiev <jrganiev@gmail.com>
2025-04-22 16:52:30 +0900

    xxx

--- 3-oy/4dars/car_price.py
+++ 3-oy/4dars/car_price.py
@@ -13,6 +13,7 @@ st.set_page_config(page_title=' app',layout='centered')
 st.title('bu app emas fffffff')
 st.markdown('bu markdown!')
 st.text('sdhsdsdhshdshdhsdhshdhsdhshdhsdhshdshdhs')
+st.time_input()
 
 print("Current dir:", os.getcwd())
 print("Files:", os.listdir('.'))
