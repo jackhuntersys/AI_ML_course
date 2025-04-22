a8da1c106373a8247057d4c2e38b623393994bad
Jamshid Ganiev <jrganiev@gmail.com>
2025-04-22 19:15:19 +0900

    aa

--- 3-oy/4dars/car_price.py
+++ 3-oy/4dars/car_price.py
@@ -7,13 +7,29 @@ from sklearn.metrics import accuracy_score
 model = load('3-oy/4dars/linear_model.joblib')
 if model:
     print('true')
-
-
-st.set_page_config(page_title=' app',layout='centered')
-st.title('bu app emas fffffff')
-st.markdown('bu markdown!')
-st.text('sdhsdsdhshdshdhsdhshdhsdhshdhsdhshdshdhs')
-st.title('dddddddd')
-
 print("Current dir:", os.getcwd())
 print("Files:", os.listdir('.'))
+
+
+st.set_page_config(page_title='Audi car price',layout='centered')
+st.title('Calculate your used car price')
+model_name = st.text_input('Car model')
+year = st.number_input("Enter a year", min_value=1900, max_value=2100, value=2025, step=1)
+transmission = st.selectbox("Transmission", ["Manual", "Automatic", "Semi-Auto"])
+mileage = st.number_input("Mileage")
+fuel = st.selectbox("Fuel type", ['Diesel', 'Petrol', 'Hybrid'])
+tax = st.number_input('Tax')
+mpg = st.number_input('MPG', 0.0)
+engine = st.number_input('Engine size', 0.0)
+
+
+input_data = pd.DataFrame([{
+    'model': model_name,
+    'year' : year,
+    'transmission': transmission,
+    'mileage': mileage,
+    'fuelType': fuel,
+    'tax':tax,
+    'mpg':mpg,
+    'engineSize': engine
+}])
